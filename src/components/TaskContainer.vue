69fe468f7058a1d8a27131ade5c7009b0fdec7c5
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2023-11-17 19:18:27 -0500

    yay, lists

--- src/components/TaskContainer.vue
+++ src/components/TaskContainer.vue
@@ -1,9 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+let id = 0;
+
+const tasks = ref([
+    { id: id++, text: "Task 1", isCompleted: false },
+    { id: id++, text: "Task 2", isCompleted: false },
+    { id: id++, text: "Task 3", isCompleted: false },
+    { id: id++, text: "Task 3", isCompleted: false }
+]);
+
+</script>
+
 <template>
     <div class="container">
         <ul>
-            <li>Task 1</li>
-            <li>Task 2</li>
-            <li>Task 3</li>
+            <li v-for="task in tasks" :key="task.id"><input type="checkbox" /> {{ task.text }}</li>
         </ul>
     </div>
 </template>
